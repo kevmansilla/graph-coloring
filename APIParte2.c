1b7320c266c07a2905955eda0ded5fc6a66d86ac
Kevin Mansilla <kevin.mansilla@mi.unc.edu.ar>
2023-04-06 00:21:46 -0300

    greedy implementation

--- APIParte2.c
+++ APIParte2.c
@@ -7,7 +7,72 @@
 
 #include "APIParte2.h"
 
-u32 Greedy(Grafo G, u32 *Orden, u32 *Color);
+/**
+ * Colorea los vertices del Grafo con un color que nunca se usa
+ */
+static void DeColorar(Grafo G, u32 *array) {
+    for (u32 i = 0u; i < NumeroDeVertices(G); i++) {
+        array[i] = UINT32_MAX;
+    }
+}
+
+u32 Greedy(Grafo G, u32 *Orden, u32 *Color) {
+    u32 k = 0u, n_colores = 1u;
+    u32 *colores_usados = NULL; // colores usados
+    u32 n_vert = NumeroDeVertices(G);
+
+    // Chequeo los parametros de la funcion
+    if (G == NULL || Orden == NULL || Color == NULL || n_vert == 0) {
+        fprintf(stderr, "ERROR");
+        return UINT32_MAX;
+    }
+    // Decolorar los vertices
+    DeColorar(G, Color);
+
+    // for para colorear
+    for (u32 i = 0u; i < n_vert; i++) {
+        // tomo el vertice de la posición k
+        u32 colores_usados_k = 0u;
+        u32 ind_vecino_k = 0u;
+        u32 color_vecino_k = 0u;
+        k = Orden[i];
+        // colores usados
+        colores_usados = calloc(n_colores, sizeof(u32));
+        if (colores_usados == NULL) {
+            fprintf(stderr, "No se pudo asignar memoria");
+            return UINT32_MAX;
+        }
+        u32 grado = Grado(k, G);
+        for (u32 j = 0u; j < grado; j++) {
+            color_vecino_k = Color[IndiceVecino(ind_vecino_k, k, G)];
+            if (color_vecino_k < n_colores && colores_usados[color_vecino_k] == 0) {
+                // significa que si esta coloreado
+                colores_usados[color_vecino_k] = 1;
+                colores_usados_k++;
+            }
+            ind_vecino_k++;
+        }
+        if (colores_usados_k == n_colores) {
+            // crear un nuevo color
+            n_colores++;
+            // Lo asigno al vertice en posicion natural K
+            Color[k] = n_colores - 1u;
+        } else {
+            // buscar el primer color libre
+            for (u32 j = 0u; j < n_colores; j++) {
+                if (colores_usados[j] == 0u) {
+                    // asignarlo al vertice en posicion natural k
+                    Color[k] = j;
+                    break;
+                }
+            }
+        }
+        // liberar colores_usados
+        free(colores_usados);
+        colores_usados = NULL;
+    }
+    return n_colores;
+}
 
 char OrdenImparPar(u32 n, u32 *Orden, u32 *Color);
 
--- main.c
+++ main.c
@@ -7,12 +7,27 @@
 #include <time.h>
 
 #include "APIG23.h"
+#include "APIParte2.h"
 
 #define BRED "\e[1;31m"
 #define BGRN "\e[1;32m"
 #define BBLU "\e[1;34m"
 #define YELL "\e[0;33m"
 #define reset "\e[0m"
+
+
+static bool esColoreoPropio(Grafo G, const u32 *coloreo) {
+    bool coloreoPropio = true;
+    u32 n = NumeroDeVertices(G);
+    for (u32 i = 0u; i < n && coloreoPropio; i++) {
+        u32 grado = Grado(i, G);
+        u32 color = coloreo[i];
+        for (u32 j = 0u; j < grado && coloreoPropio; j++) {
+            coloreoPropio = (color != coloreo[IndiceVecino(j, i, G)]);
+        }
+    }
+    return coloreoPropio;
+}
 
 int main() {
     clock_t begin = clock();
@@ -29,17 +44,62 @@ int main() {
     // ----------------- Primera Parte -----------------
     // -------------------------------------------------
 
-    printf(BBLU ">>>> Probando parte I\n" reset);
-    printf(YELL ">>>> Estructura del grafo\n" reset);
-    printf("El número de vertices del grafo es: %u\n",
-           NumeroDeVertices(G));
-    printf("El número de lados del grafo es: %u\n", NumeroDeLados(G));
-    printf("El Delta del grafo es: %u.\n", Delta(G));
-    printf(YELL">>>> indiceONvecino\n" reset);
-    printf("indic k: %u, vertice: %u\n", 4u, Nombre(4, G));
-    printf("vecinoIndice j: %u, vecino: %u\n", 0u, IndiceVecino(0, 4, G));
-    printf("vecinoIndice j: %u, vecino: %u\n", 1u, IndiceVecino(1, 4, G));
-    printf("vecinoIndice j: %u, vecino: %u\n", 2u, IndiceVecino(2, 4, G));
+    // printf(BBLU ">>>> Probando parte I\n" reset);
+    // printf(YELL ">>>> Estructura del grafo\n" reset);
+    // printf("El número de vertices del grafo es: %u\n",
+    //        NumeroDeVertices(G));
+    // printf("El número de lados del grafo es: %u\n", NumeroDeLados(G));
+    // printf("El Delta del grafo es: %u.\n", Delta(G));
+    // printf(YELL">>>> indiceONvecino\n" reset);
+    // printf("indic k: %u, vertice: %u\n", 4u, Nombre(4, G));
+    // printf("vecinoIndice j: %u, vecino: %u\n", 0u, IndiceVecino(0, 4, G));
+    // printf("vecinoIndice j: %u, vecino: %u\n", 1u, IndiceVecino(1, 4, G));
+    // printf("vecinoIndice j: %u, vecino: %u\n", 2u, IndiceVecino(2, 4, G));
+
+    
+    // -------------------------------------------------
+    // ----------------- Segunda Parte -----------------
+    // -------------------------------------------------
+
+    printf(BBLU ">>>> Probando parte II\n" reset);
+    //----------------- GREEDY -----------------
+    printf(">>>> GREEDY\n");
+
+    // Crear arreglo orden e inicializar con los indices de los vertices
+    u32 *orden = calloc(G->vertices_num, sizeof(u32));
+    for (u32 i = 0u; i < G->vertices_num; i++) {
+        orden[i] = i;
+    }
+    // Arreglo de colores
+    u32 *coloreo = calloc(G->vertices_num, sizeof(u32));
+
+    // Greedy
+    u32 nColores = Greedy(G, orden, coloreo);
+    printf("Greedy: %u\n", nColores);
+
+    // 1000 greedys
+    // u32 indice = 0u;
+    // for (u32 i = 0u; i < 1000; i++) {
+    //     u32 nColores = Greedy(G, orden, coloreo);
+    //     indice++;
+    //     printf("Greedy: %u, Nro: %u\n", nColores, indice);
+    // }
+
+    // Chequeo si el coloreo es propio
+    if(esColoreoPropio(G, coloreo) == 1){
+        printf(BGRN "test: OK\n" reset);
+    }
+    else{
+        printf(BRED "test: NO\n" reset);
+    }
+
+    // libero
+    free(orden);
+    orden = NULL;
+    free(coloreo);
+    coloreo = NULL;
+
+    DestruirGrafo(G);
 
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
@@ -48,6 +108,5 @@ int main() {
     printf(BRED "El tiempo de corrida del programa fue de: %fs\n", time_spent);
     printf("Nos vemos ...\n" reset);
 
-    DestruirGrafo(G);
     return 0;
 }
