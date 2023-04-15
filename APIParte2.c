4f81c1b289f8562db25c1b9dad5295971cdd7312
Kevin Mansilla <kevin.mansilla@mi.unc.edu.ar>
2023-04-15 18:47:17 -0300

    changes greedy

--- APIParte2.c
+++ APIParte2.c
@@ -10,72 +10,122 @@
 /**
  * Colorea los vertices del Grafo con un color que nunca se usa
  */
-static void DeColorar(Grafo G, u32 *array) {
-    for (u32 i = 0u; i < NumeroDeVertices(G); i++) {
-        array[i] = UINT32_MAX;
-    }
-}
+// static void DeColorar(Grafo G, u32 *array) {
+//     for (u32 i = 0u; i < NumeroDeVertices(G); i++) {
+//         array[i] = UINT32_MAX;
+//     }
+// }
 
 /**
  * Greedy
  */
 u32 Greedy(Grafo G, u32 *Orden, u32 *Color) {
-    u32 k = 0u, n_colores = 1u;
-    u32 *colores_usados = NULL; // colores usados
     u32 n_vert = NumeroDeVertices(G);
-
-    // Chequeo los parametros de la funcion
-    if (G == NULL || Orden == NULL || Color == NULL || n_vert == 0) {
-        fprintf(stderr, "ERROR");
+    u32 n_colores = 1;
+    u32 *colores_usados = calloc(n_vert, sizeof(u32));
+    if (colores_usados == NULL) {
+        fprintf(stderr, "No se pudo asignar memoria");
         return UINT32_MAX;
     }
     // Decolorar los vertices
-    DeColorar(G, Color);
+    memset(Color, UINT32_MAX, n_vert * sizeof(u32));
 
     // for para colorear
-    for (u32 i = 0u; i < n_vert; i++) {
+    for (u32 i = 0; i < n_vert; i++) {
         // tomo el vertice de la posición k
-        u32 colores_usados_k = 0u;
-        u32 ind_vecino_k = 0u;
-        u32 color_vecino_k = 0u;
-        k = Orden[i];
-        // colores usados
-        colores_usados = calloc(n_colores, sizeof(u32));
-        if (colores_usados == NULL) {
-            fprintf(stderr, "No se pudo asignar memoria");
-            return UINT32_MAX;
-        }
+        u32 k = Orden[i];
         u32 grado = Grado(k, G);
-        for (u32 j = 0u; j < grado; j++) {
-            color_vecino_k = Color[IndiceVecino(ind_vecino_k, k, G)];
-            if (color_vecino_k < n_colores && colores_usados[color_vecino_k] == 0) {
-                // significa que si esta coloreado
+        memset(colores_usados, 0, n_colores * sizeof(u32));
+        u32 ind_vecino_k = 0;
+        for (u32 j = 0; j < grado; j++) {
+            u32 color_vecino_k = Color[IndiceVecino(ind_vecino_k, k, G)];
+            if (color_vecino_k < n_colores) {
                 colores_usados[color_vecino_k] = 1;
-                colores_usados_k++;
             }
             ind_vecino_k++;
         }
-        if (colores_usados_k == n_colores) {
-            // crear un nuevo color
-            n_colores++;
-            // Lo asigno al vertice en posicion natural K
-            Color[k] = n_colores - 1u;
-        } else {
-            // buscar el primer color libre
-            for (u32 j = 0u; j < n_colores; j++) {
-                if (colores_usados[j] == 0u) {
-                    // asignarlo al vertice en posicion natural k
-                    Color[k] = j;
-                    break;
-                }
+        u32 color_k;
+        for (color_k = 0; color_k < n_colores; color_k++) {
+            if (colores_usados[color_k] == 0) {
+                Color[k] = color_k;
+                break;
             }
         }
-        // liberar colores_usados
-        free(colores_usados);
-        colores_usados = NULL;
+        if (color_k == n_colores) {
+            n_colores++;
+            Color[k] = color_k;
+        }
     }
+    free(colores_usados);
     return n_colores;
 }
+
+// u32 Greedy(Grafo G, u32 *Orden, u32 *Color)
+// {
+//     u32 n_colores = 1;
+//     u32 n_vert = NumeroDeVertices(G);
+
+//     // Chequeo los parametros de la funcion
+//     if (!G || !Orden || !Color || !n_vert)
+//     {
+//         fprintf(stderr, "ERROR");
+//         return UINT32_MAX;
+//     }
+//     // Decolorar los vertices
+//     DeColorar(G, Color);
+
+//     // for para colorear
+//     for (u32 i = 0; i < n_vert; i++)
+//     {
+//         // tomo el vertice de la posición k
+//         u32 colores_usados_k = 0;
+//         u32 ind_vecino_k = 0;
+//         u32 color_vecino_k = 0;
+//         u32 k = Orden[i];
+//         // colores usados
+//         u32 *colores_usados = calloc(n_colores, sizeof(u32));
+//         if (!colores_usados)
+//         {
+//             fprintf(stderr, "No se pudo asignar memoria");
+//             return UINT32_MAX;
+//         }
+//         u32 grado = Grado(k, G);
+//         for (u32 j = 0; j < grado; j++)
+//         {
+//             color_vecino_k = Color[IndiceVecino(ind_vecino_k, k, G)];
+//             if (color_vecino_k < n_colores && !colores_usados[color_vecino_k])
+//             {
+//                 // significa que si esta coloreado
+//                 colores_usados[color_vecino_k] = 1;
+//                 colores_usados_k++;
+//             }
+//             ind_vecino_k++;
+//         }
+//         if (colores_usados_k == n_colores)
+//         {
+//             // crear un nuevo color
+//             n_colores++;
+//             // Lo asigno al vertice en posicion natural K
+//             Color[k] = n_colores - 1;
+//         }
+//         else
+//         {
+//             // buscar el primer color libre
+//             for (u32 j = 0; j < n_colores; j++)
+//             {
+//                 if (!colores_usados[j])
+//                 {
+//                     // asignarlo al vertice en posicion natural k
+//                     Color[k] = j;
+//                     break;
+//                 }
+//             }
+//         }
+//         // liberar colores_usados
+//         free(colores_usados);
+//     }
+//     return n_colores;
+// }
 
 /**
  * TAD para funciones de crear ordenes
